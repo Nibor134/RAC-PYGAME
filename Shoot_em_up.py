3da5851973afa2fd4bbaf74f1b4f290df1aa21f7
Robin-0909114 <Winkels.R1@gmail.com>
2022-10-05 13:18:33 +0200

    Set up main game

---
+++ Shoot_em_up.py
@@ -0,0 +1,14 @@
+# Import and initialize all librarys
+import pygame
+pygame.init()
+
+import test.Display_settings
+import test.Timerfunction 
+
+# Set up the drawing window
+screen = test.Display_settings
+
+# Set up Main Menu
+Time = test.Timerfunction
+
+# Set up Game Mode
