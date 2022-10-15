ea0420149648ca3cad896d9c42d4e563744e5128
Robin-0909114 <Winkels.r1@gmail.com>
2022-10-15 14:00:37 +0200

    cleaned up repo

--- Lib/Main Menu.py
+++ Lib/Main Menu.py
@@ -7,7 +7,7 @@ SCREEN = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption("Menu")
 
 BG = pygame.image.load("Lib/img/space.png")
-HS = pygame.image.load("Lib/img/Galaxyback.png")
+HS = pygame.image.load("Lib/img/Startmenu.jpg")
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("Lib/img/Cyberpunk.ttf", size)
